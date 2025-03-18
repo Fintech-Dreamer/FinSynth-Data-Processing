790c983908d1e8fa8508717dc23d6a2f30386122
Str0keOOOO <786619108@qq.com>
2025-03-18 11:55:52 +0800

    feat:整理文档结构

---
+++ params_example.py
@@ -0,0 +1,8 @@
+# 存放ChatOpenAI的密钥和API地址还有模型名称
+# --------------------------------------------------------------------     
+# Qwen api
+API_KEY = "******"
+API_BASE = "https://dashscope.aliyuncs.com/compatible-mode/v1"
+MODEL = "qwen-max"
+MODEL_PICTURE = "qwen-vl-plus"
+# --------------------------------------------------------------------
